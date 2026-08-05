b0597eac2b42809e4e1ccad0444835d09040198d
unknown <hinslmh1606@gmail.com>
2026-08-05 09:10:48 +0800

    add

---
+++ formatting test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<p><b>welcome to HTML Formatting Training</b></p>
+
+<p><i>HTML</i> is easy to learn.</p>
+<p>Practice every day to improve your <ins>skills</ins>.</p>
+
+<p>Water formula is H<sup>2</sup>O.</p>
+
+<p>Einstien created E = mc<sub>2</sub>.</p>
+
+<p><mark>Important Notice: Submit your assignment today.</mark></p>
+
+<hr>
+
+<p><del>Old price:RM100</del></p>
+<p><ins>New Price:RM80</ins></p>
+</body>
+</html>
